02ad5cdf018bc1ec1171a1e4115b26d5e73794d6
Chendasum <chendasum168@gmail.com>
2025-08-25 17:09:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -132,6 +132,7 @@ const creditAssessment = require('./cambodia/creditAssessment');
 const loanOrigination = require('./cambodia/loanOrigination');
 const loanServicing = require('./cambodia/loanServicing'); // 📋 NEW: Loan servicing & collections
 const riskManagement = require('./cambodia/riskManagement'); // 🚨 NEW: Risk assessment & monitoring
+const loanRecovery = require('./cambodia/loanRecovery'); // 💰 NEW: Collections & asset recovery
 
 // 📊 DATABASE & MEMORY SYSTEM with Fallback Protection
 let database, memory, logger;
